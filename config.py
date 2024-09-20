95be52c79f9c328d500a15556473ba97051587c3
Mehdi Afshari <afshari.mehdi@gmail.com>
2024-09-19 18:32:17 -0700

    Configs commented

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+"""API URLS Configs"""
 API_BASE_URL = "http://localhost:3123/animals/v1/animals"
 API_DETAILS_BASE_URL = "http://localhost:3123/animals/v1/animals/{}"
 API_HOME_URL = "http://localhost:3123/animals/v1/home"
 
+"""FETCH and POST Configs"""
 GET_ANIMAL_DETAILS_MAX_RETRIES = 15+1
 GET_HEALTHY_RESPONSE_CODE = 200
 POST_BATCH_SIZE = 100
